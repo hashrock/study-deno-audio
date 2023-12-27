e4c57e0de9a69c67e230c59588e9c55e27e85b4f
hashrock <hashrock@users.noreply.github.com>
2023-12-27 17:58:20 +0900

    update

--- main.ts
+++ main.ts
@@ -45,10 +45,49 @@ function osc(t: number, freq: number, amp = 1) {
 
 let t = 0;
 
+function envelope(t: number, dur: number) {
+  const attack = 0;
+  const decay = 0.1;
+  const sustain = 0;
+  const release = 0;
+
+  if (t < attack) {
+    return t / attack;
+  } else if (t < attack + decay) {
+    return 1 - ((t - attack) / decay) * (1 - sustain);
+  } else if (t < dur - release) {
+    return sustain;
+  } else if (t < dur) {
+    return sustain * (1 - (t - (dur - release)) / release);
+  } else {
+    return 0;
+  }
+}
+
+function tune(msec: number) {
+  const notes = [
+    { freq: 440, start: 0, dur: 1 },
+    { freq: 880, start: 1, dur: 1 },
+    { freq: 440, start: 2, dur: 1 },
+    { freq: 880, start: 3, dur: 1 },
+  ];
+
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i];
+    console.log(note);
+    if (msec >= note.start * 1000 && msec < (note.start + note.dur) * 1000) {
+      const t = msec - note.start * 1000;
+      const amp = envelope(t, note.dur * 1000);
+      return osc(msec, note.freq, amp);
+    }
+  }
+  return 0;
+}
+
 while (true) {
   for (let i = 0; i < FRAMES_PER_BUFFER; i++) {
     const msec = t / SAMPLE_RATE;
-    buffer[i * 2] = osc(msec, 880, 1);
+    buffer[i * 2] = tune(msec);
     t++;
   }
   PortAudio.writeStream(stream, buffer, FRAMES_PER_BUFFER * 2);
